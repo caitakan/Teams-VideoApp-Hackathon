058c72419f102496a974637cf6f43fdb3dbba6e0
Jason Zhao <haizhao@microsoft.com>
2021-10-14 16:07:49 -0700

    nit

--- app/index.js
+++ app/index.js
@@ -15,7 +15,7 @@ let errorOccurs = false;
 //Sample video effect
 function videoFrameHandler(videoFrame, notifyVideoProcessed, notifyError) {
   let colorType = Math.ceil(Math.random() * 10) % 2 > 0;
-  let colorOffset = colorType ? 125 : 55;
+  let colorOffset = colorType ? 300 : 200;
 
       for (let i = 0; i < videoFrame.data.length; i++) {
         // Invert the colors
